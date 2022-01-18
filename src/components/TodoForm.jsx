1dbbb5fe17ff4880c381113399f38d0fe8d3837e
valcaro87@gmail.com <valcaro87@gmail.com>
2022-01-18 14:37:09 +0800

    @min 1:07:00

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Form, Row, Col, Button, Input } from 'antd';
-import {PlusCircleFilled} from '@ant-design/icons';
-import { render } from '@testing-library/react';
+import React from "react";
+import { Form, Row, Col, Button, Input } from "antd";
+import { PlusCircleFilled } from "@ant-design/icons";
+import { render } from "@testing-library/react";
 
 const TodoForm = ([onFormSubmit]) => {
-    const [form] = Form.useForm();
-    
-    const onFinish = () => {
-        onFormSubmit({
-            title: form.getFieldValue('title'),
-            completed: false
-        });
-        console.log(form.getFieldValue('title'));
-        form.resetFields();
-    }
+  const [form] = Form.useForm();
 
-    return(
-        <Form
-            form={form}
-            onFinish={onFinish}
-            layout="horizontal"
-            className="todo-form">
-            
-            <Row gutter={20}>
-                <Col xs={24} sm={24} md={17} lg={19} xl={20}>
-                    <Form.Item
-                        name={'title'}
-                        rules={[{ required: true, message: 'This field is required' }]}>
-                        <Input placeholder="What needs to be done?" />
-                    </Form.Item>
-                </Col>
-                <Col xs={24} sm={24} md={7} lg={5} xl={4}>
-                    <Button type="primary" htmlType="submit" bloack>
-                    <PlusCircleFilled />
-                        Add Todo
-                    </Button>
-                </Col>
-            </Row>
+  const onFinish = () => {
+    onFormSubmit({
+      title: form.getFieldValue("title"),
+      completed: false,
+    });
+    console.log(form.getFieldValue("title"));
+    form.resetFields();
+  };
 
-        </Form>
-    );
-}
+  return (
+    <Form
+      form={form}
+      onFinish={onFinish}
+      layout="horizontal"
+      className="todo-form"
+    >
+      <Row gutter={20}>
+        <Col xs={24} sm={24} md={17} lg={19} xl={20}>
+          <Form.Item
+            name={"title"}
+            rules={[{ required: true, message: "This field is required" }]}
+          >
+            <Input placeholder="What needs to be done?" />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={7} lg={5} xl={4}>
+          <Button type="primary" htmlType="submit" block>
+            <PlusCircleFilled />
+            Add Todo
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
 
 export default TodoForm;
---
+++ src/components/TodoTab.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { Tabs, Layout, Row, Col, List } from "antd";
+import TodoItem from "./TodoItem";
+
+const TodoTab = ({ todos, onTodoRemoval, onTodoToggle }) => {
+  return (
+    <>
+      <List></List>
+    </>
+  );
+};
+
+export default TodoTab;
